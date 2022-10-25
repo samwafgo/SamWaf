0a49a88892736ba944ad2781a999b165f1cd0b99
samwaf <samwafgo@gmail.com>
2022-10-25 17:51:37 +0800

    增加编译

---
+++ build-releases.bat
@@ -0,0 +1,15 @@
+SET CGO_ENABLED=0
+SET GOOS=windows
+SET GOARCH=amd64
+go build -o %cd%/release/SamWaf64.exe main.go localdb.go localserver.go wafengine.go
+
+SET CGO_ENABLED=0
+SET GOOS=windows
+SET GOARCH=386
+go build -o %cd%/release/SamWaf32.exe main.go localdb.go localserver.go wafengine.go
+
+SET CGO_ENABLED=0
+SET GOOS=linux
+SET GOARCH=amd64
+go build -o %cd%/release/SamWafLinux64.exe main.go localdb.go localserver.go wafengine.go
+

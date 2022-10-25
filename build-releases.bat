04550bd7c360736301b7255587268e6977a75614
samwaf <samwafgo@gmail.com>
2022-10-25 17:52:54 +0800

    linxu编译

--- build-releases.bat
+++ build-releases.bat
@@ -11,5 +11,5 @@ go build -o %cd%/release/SamWaf32.exe main.go localdb.go localserver.go wafengin
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=amd64
-go build -o %cd%/release/SamWafLinux64.exe main.go localdb.go localserver.go wafengine.go
+go build -o %cd%/release/SamWafLinux64 main.go localdb.go localserver.go wafengine.go
 

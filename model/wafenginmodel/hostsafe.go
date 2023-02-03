3b913882c1233a1bc6a8b980e0249483d978794b
caipeng <530566495@qq.com>
2023-02-03 22:28:16 +0800

    优化部分waf引擎代码部分

---
+++ model/wafenginmodel/hostsafe.go
@@ -0,0 +1,25 @@
+package wafenginmodel
+
+import (
+	"SamWaf/model"
+	"SamWaf/plugin"
+	"SamWaf/utils"
+	"net/http/httputil"
+)
+
+// 主机安全配置
+type HostSafe struct {
+	RevProxy            *httputil.ReverseProxy
+	Rule                *utils.RuleHelper
+	TargetHost          string
+	RuleData            []model.Rules
+	RuleVersionSum      int //规则版本的汇总 通过这个来进行版本动态加载
+	Host                model.Hosts
+	PluginIpRateLimiter *plugin.IPRateLimiter //ip限流
+	IPWhiteLists        []model.IPWhiteList   //ip 白名单
+	UrlWhiteLists       []model.URLWhiteList  //url 白名单
+	LdpUrlLists         []model.LDPUrl        //url 隐私保护
+
+	IPBlockLists  []model.IPBlockList  //ip 黑名单
+	UrlBlockLists []model.URLBlockList //url 黑名单
+}

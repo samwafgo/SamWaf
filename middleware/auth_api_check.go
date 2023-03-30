fa781f15c454f7419d95a7dcb40919c3de050141
caipeng <530566495@qq.com>
2023-03-30 15:03:52 +0800

    去掉gin中间件固定debug模式打印token

--- middleware/auth_api_check.go
+++ middleware/auth_api_check.go
@@ -12,13 +12,13 @@ func Auth() gin.HandlerFunc {
 		// 获取请求头中 token，实际是一个完整被签名过的 token；a complete, signed token
 		tokenStr := c.GetHeader("X-Token")
 		if tokenStr == "" {
-			zlog.Info("无token")
+			zlog.Debug("无token")
 
 			response.AuthFailWithMessage("鉴权失败", c)
 			c.Abort()
 			return
 		}
-		zlog.Info("有token:" + tokenStr)
+		zlog.Debug("有token:" + tokenStr)
 		// 将 claims 中的用户信息存储在 context 中
 		//c.Set("userId", claims.UserId)
 

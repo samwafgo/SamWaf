14104e2cc2784bbbf96506393943d7a25a61911c
caipeng <530566495@qq.com>
2023-06-04 18:06:20 +0800

    新增系统配置

--- model/request/waf_system_config_add_req.go
+++ model/request/waf_system_config_add_req.go
@@ -1,7 +1,7 @@
 package request
 
 type WafSystemConfigAddReq struct {
-	Item    string `json:"item" form:"item"`      //item
-	Value   string `json:"value" form:"value"`    //value
-	Remarks string `json:"remark" form:"remarks"` //备注
+	Item    string `json:"item" form:"item"`       //item
+	Value   string `json:"value" form:"value"`     //value
+	Remarks string `json:"remarks" form:"remarks"` //备注
 }

cbbe870ba4c4ac44586b986f796287cdf14f0e26
caipeng <530566495@qq.com>
2023-07-11 15:48:28 +0800

    去掉定时任务触发的记录

--- wafenginecore/wafengine.go
+++ wafenginecore/wafengine.go
@@ -431,7 +431,7 @@ func (waf *WafEngine) modifyResponse() func(*http.Response) error {
 			global.GQEQUE_LOG_DB.PushBack(weblogbean)
 		}
 		//TODO 如果是指定URL 或者 IP 不记录日志
-		if !isStaticAssist && weblogbean.URL != "/index.php/lttshop/task_scheduling/orderSale" && weblogbean.URL != "/index.php/lttshop/task_scheduling/orderHotSale" {
+		if !isStaticAssist && !strings.HasPrefix(weblogbean.URL, "/index.php/lttshop/task_scheduling/") {
 			weblogbean.ACTION = "放行"
 			global.GQEQUE_LOG_DB.PushBack(weblogbean)
 		}

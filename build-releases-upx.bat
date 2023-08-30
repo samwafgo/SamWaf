37efacdc97f81a2b237d9bfaf1be67fac1a5b014
caipeng <530566495@qq.com>
2023-08-30 15:20:45 +0800

    update:win更新版本20230830

--- build-releases-upx.bat
+++ build-releases-upx.bat
@@ -2,4 +2,4 @@ SET CGO_ENABLED=1
 SET GOOS=windows
 SET GOARCH=amd64
 SET GIN_MODE=release
-go build -ldflags="-X SamWaf/global.GWAF_RELEASE=true -X SamWaf/global.GWAF_RELEASE_VERSION_NAME=20230619 -X SamWaf/global.GWAF_RELEASE_VERSION=101 -s -w" -o %cd%/release/SamWaf64.exe main.go && %cd%/upx/win64/upx -9  %cd%/release/SamWaf64.exe
+go build -ldflags="-X SamWaf/global.GWAF_RELEASE=true -X SamWaf/global.GWAF_RELEASE_VERSION_NAME=20230830 -X SamWaf/global.GWAF_RELEASE_VERSION=113 -s -w" -o %cd%/release/SamWaf64.exe main.go && %cd%/upx/win64/upx -9  %cd%/release/SamWaf64.exe

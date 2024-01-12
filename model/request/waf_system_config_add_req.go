de7dcd87dd75f406c89402689380633893e22304
samwaf <samwafgo@gmail.com>
2024-01-12 10:12:55 +0800

    feat:配置增加可选类型

--- model/request/waf_system_config_add_req.go
+++ model/request/waf_system_config_add_req.go
@@ -1,7 +1,9 @@
 package request
 
 type WafSystemConfigAddReq struct {
-	Item    string `json:"item" form:"item"`       //item
-	Value   string `json:"value" form:"value"`     //value
-	Remarks string `json:"remarks" form:"remarks"` //备注
+	Item     string `json:"item" form:"item"`           //item
+	Value    string `json:"value" form:"value"`         //value
+	ItemType string `json:"item_type" form:"item_type"` //item_type
+	Options  string `json:"options" form:"options"`     //options
+	Remarks  string `json:"remarks" form:"remarks"`     //备注
 }
--- model/request/waf_system_config_edit_req.go
+++ model/request/waf_system_config_edit_req.go
@@ -1,8 +1,10 @@
 package request
 
 type WafSystemConfigEditReq struct {
-	Id      string `json:"id"`
-	Item    string `json:"item" form:"item"`       //item
-	Value   string `json:"value" form:"value"`     //value
-	Remarks string `json:"remarks" form:"remarks"` //备注
+	Id       string `json:"id"`
+	Item     string `json:"item" form:"item"`           //item
+	Value    string `json:"value" form:"value"`         //value
+	ItemType string `json:"item_type" form:"item_type"` //item_type
+	Options  string `json:"options" form:"options"`     //options
+	Remarks  string `json:"remarks" form:"remarks"`     //备注
 }

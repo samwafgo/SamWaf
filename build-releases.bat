a1276a948dde981e1eb0d3051ff2fc351235944a
samwaf <samwafgo@gmail.com>
2024-01-18 12:49:22 +0800

    feat:资源优化

--- build-releases.bat
+++ build-releases.bat
@@ -2,4 +2,4 @@ SET CGO_ENABLED=1
 SET GOOS=windows
 SET GOARCH=amd64
 SET GIN_MODE=release
-go build -ldflags="-X SamWaf/global.GWAF_RELEASE=true -X SamWaf/global.GWAF_RELEASE_VERSION_NAME=20230905 -X SamWaf/global.GWAF_RELEASE_VERSION=115 -s -w" -o %cd%/release/SamWaf64.exe main.go
+go build -ldflags="-X SamWaf/global.GWAF_RELEASE=true -X SamWaf/global.GWAF_RELEASE_VERSION_NAME=20231128 -X SamWaf/global.GWAF_RELEASE_VERSION=v1.0.128 -s -w" -o %cd%/release/SamWaf64.exe main.go

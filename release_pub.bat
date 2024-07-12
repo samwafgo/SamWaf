6ea074e320791efc2d0d12e1794a14f4a75b03e4
samwaf <samwafgo@gmail.com>
2024-07-12 16:50:11 +0800

    update:(20240712) v1.2.1  管理中心和授权体系

--- release_pub.bat
+++ release_pub.bat
@@ -1,7 +1,7 @@
 @echo on
 chcp 65001
 set currentpath=%cd%
-set currentversion=v1.1.10
-set currentdescription=新版增加添加指定url不进行日志记录功能，建议升级
+set currentversion=v1.2.1
+set currentdescription=新版修正安全问题，请尽快安排升级
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform windows-amd64 %currentpath%\release\SamWaf64.exe %currentversion% 
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform linux-amd64 %currentpath%\release\SamWafLinux64 %currentversion%

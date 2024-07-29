3e78fd50457afbc5c6ddb5cf45f11cd422d2ee31
samwaf <samwafgo@gmail.com>
2024-07-29 15:16:42 +0800

    update:(20240729) v1.2.2 修正兼容问题

--- release_pub.bat
+++ release_pub.bat
@@ -1,7 +1,7 @@
 @echo on
 chcp 65001
 set currentpath=%cd%
-set currentversion=v1.2.1
-set currentdescription=新版修正安全问题，请尽快安排升级
+set currentversion=v1.2.2
+set currentdescription=新版修正兼容问题
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform windows-amd64 %currentpath%\release\SamWaf64.exe %currentversion% 
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform linux-amd64 %currentpath%\release\SamWafLinux64 %currentversion%

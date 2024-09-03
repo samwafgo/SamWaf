baaf795b0c613de016edbcf645632dcf4e1c08a3
samwaf <samwafgo@gmail.com>
2024-09-03 15:28:33 +0800

    fix:update description

--- release_pub.bat
+++ release_pub.bat
@@ -2,6 +2,6 @@
 chcp 65001
 set currentpath=%cd%
 set currentversion=v1.3.1
-set currentdescription=新版修正兼容问题
+set currentdescription=新增多语言
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform windows-amd64 %currentpath%\release\SamWaf64.exe %currentversion% 
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform linux-amd64 %currentpath%\release\SamWafLinux64 %currentversion%

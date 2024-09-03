fda2faa4ed78105dddfe5df7dd18e373b7e1b6db
samwaf <samwafgo@gmail.com>
2024-09-03 14:57:20 +0800

    update:(20240903) v1.3.1 add chinese/english language

--- release_pub.bat
+++ release_pub.bat
@@ -1,7 +1,7 @@
 @echo on
 chcp 65001
 set currentpath=%cd%
-set currentversion=v1.2.2
+set currentversion=v1.3.1
 set currentdescription=新版修正兼容问题
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform windows-amd64 %currentpath%\release\SamWaf64.exe %currentversion% 
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform linux-amd64 %currentpath%\release\SamWafLinux64 %currentversion%

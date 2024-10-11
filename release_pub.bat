01131392feda98e870b5fdb1c26333c224cbd1b3
samwaf <samwafgo@gmail.com>
2024-10-11 09:21:42 +0800

    update:(20241011) v1.3.6 add some feature

--- release_pub.bat
+++ release_pub.bat
@@ -1,7 +1,7 @@
 @echo on
 chcp 65001
 set currentpath=%cd%
-set currentversion=v1.3.3
-set currentdescription=请在闲时升级,新增访客Proxy自定义header等
+set currentversion=v1.3.6
+set currentdescription=请在闲时升级,新增ssl证书夹,自定义ip库等
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform windows-amd64 %currentpath%\release\githubci\%currentversion%\SamWaf64.exe %currentversion%
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform linux-amd64 %currentpath%\release\githubci\%currentversion%\SamWafLinux64 %currentversion%

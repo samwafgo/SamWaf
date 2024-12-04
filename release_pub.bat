60f85ff2935289079c3ccfe00dea485d9164b1f2
samwaf <samwafgo@gmail.com>
2024-12-04 08:46:03 +0800

    update:(20241204) v1.3.8 add some feature

--- release_pub.bat
+++ release_pub.bat
@@ -1,7 +1,7 @@
 @echo on
 chcp 65001
 set currentpath=%cd%
-set currentversion=v1.3.7
+set currentversion=v1.3.8
 set currentdescription=请在闲时升级,提升稳定性新增多项功能，查看详情https://doc.samwaf.com/quickstart/Update.html
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform windows-amd64 %currentpath%\release\githubci\%currentversion%\SamWaf64.exe %currentversion%
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform linux-amd64 %currentpath%\release\githubci\%currentversion%\SamWafLinux64 %currentversion%

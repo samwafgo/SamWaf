44394af8ae3a00d7c09f21f2c48e5c002cdfd9ca
samwaf <samwafgo@gmail.com>
2025-05-27 08:41:56 +0800

    update:(20250527) v1.3.14 upgrade and optimize

--- build-releases-win7-upx.bat
+++ build-releases-win7-upx.bat
@@ -2,4 +2,4 @@ SET CGO_ENABLED=1
 SET GOOS=windows
 SET GOARCH=amd64
 SET GIN_MODE=release
-go build -ldflags="-X SamWaf/global.GWAF_RUNTIME_WIN7_VERSION=true -X SamWaf/global.GWAF_RELEASE=true -X SamWaf/global.GWAF_RELEASE_VERSION_NAME=20250504 -X SamWaf/global.GWAF_RELEASE_VERSION=v1.3.13 -s -w" -o %cd%/release/SamWaf64.exe main.go && %cd%/upx/win64/upx -9  %cd%/release/SamWaf64.exe
+go build -ldflags="-X SamWaf/global.GWAF_RUNTIME_WIN7_VERSION=true -X SamWaf/global.GWAF_RELEASE=true -X SamWaf/global.GWAF_RELEASE_VERSION_NAME=20250527 -X SamWaf/global.GWAF_RELEASE_VERSION=v1.3.14 -s -w" -o %cd%/release/SamWaf64.exe main.go && %cd%/upx/win64/upx -9  %cd%/release/SamWaf64.exe

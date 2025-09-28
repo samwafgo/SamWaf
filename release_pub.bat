9934d85899895569047459beb9261fff5338d093
samwaf <samwafgo@gmail.com>
2025-09-28 09:28:13 +0800

    update:(20250928) v1.3.16 upgrade and optimize

--- release_pub.bat
+++ release_pub.bat
@@ -1,7 +1,7 @@
 @echo on
 chcp 65001
 set currentpath=%cd%
-set currentversion=v1.3.15
+set currentversion=v1.3.16
 set currentdescription=请在闲时升级,修复bug,新增多项功能，升级前先查看详情，https://doc.samwaf.com/quickstart/Update.html
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform windows-amd64 %currentpath%\release\githubci\%currentversion%\SamWaf64.exe %currentversion%
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform linux-amd64 %currentpath%\release\githubci\%currentversion%\SamWafLinux64 %currentversion%

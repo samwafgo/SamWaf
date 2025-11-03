de39a94694334e6de8656ab33a5ae7b797d5b3f7
samwaf <samwafgo@gmail.com>
2025-11-03 14:25:39 +0800

    update:(20251103) v1.3.17 upgrade and optimize

--- release_pub.bat
+++ release_pub.bat
@@ -1,8 +1,8 @@
 @echo on
 chcp 65001
 set currentpath=%cd%
-set currentversion=v1.3.16
-set currentdescription=请在闲时升级,修复bug,新增多项功能，升级前先查看详情，https://doc.samwaf.com/quickstart/Update.html
+set currentversion=v1.3.17
+set currentdescription=请在闲时升级,新增提升稳定性修复bug，升级前先查看详情，https://doc.samwaf.com/quickstart/Update.html
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform windows-amd64 %currentpath%\release\githubci\%currentversion%\SamWaf64.exe %currentversion%
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\samwaf_update -platform linux-amd64 %currentpath%\release\githubci\%currentversion%\SamWafLinux64 %currentversion%
 %currentpath%\setup\go_gen_updatefile\go_gen_updatefile.exe -desc %currentdescription% -o %currentpath%\release\web\win7\samwaf_update -platform windows-amd64 %currentpath%\release\githubci\%currentversion%\SamWaf64ForWin7Win8Win2008.exe %currentversion%

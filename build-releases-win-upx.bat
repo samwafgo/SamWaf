6be112a17cfbb32d46b50b59152596371d5a178c
samwaf <samwafgo@gmail.com>
2026-07-21 23:59:09 +0800

    update:(20260721) v1.3.23 fix bug

--- build-releases-win-upx.bat
+++ build-releases-win-upx.bat
@@ -2,4 +2,4 @@ SET CGO_ENABLED=1
 SET GOOS=windows
 SET GOARCH=amd64
 SET GIN_MODE=release
-go build -ldflags="-X SamWaf/global.GWAF_RELEASE=true -X SamWaf/global.GWAF_RELEASE_VERSION_NAME=20260720 -X SamWaf/global.GWAF_RELEASE_VERSION=v1.3.22 -s -w" -o %cd%/release/SamWaf64.exe ./cmd/samwaf/main.go
+go build -ldflags="-X SamWaf/global.GWAF_RELEASE=true -X SamWaf/global.GWAF_RELEASE_VERSION_NAME=20260721 -X SamWaf/global.GWAF_RELEASE_VERSION=v1.3.23 -s -w" -o %cd%/release/SamWaf64.exe ./cmd/samwaf/main.go
